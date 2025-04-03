4cfeccdc5fa138c192eeb1b8fc0bdb8f9b8c26c4
YohaniPa <YohaniPa>
2025-04-03 07:59:43 -0700

    se modifico

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const axios = require('axios');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
-// ruta  (e.g., HTML, CSS)
+// Servir archivos estáticos desde la carpeta "public"
 app.use(express.static('public'));
 
-// obtener datos de un Pokémon por su nombre
+// Servir index.html cuando se acceda a la raíz
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Obtener datos de un Pokémon por su nombre
 app.get('/pokemon/:name', async (req, res) => {
   const { name } = req.params;
-  
+
   try {
     const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
     res.json(response.data);
